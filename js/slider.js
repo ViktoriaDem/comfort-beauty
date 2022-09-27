6499a2660e3dbe68e71c9bc7061e753ca932a57c
Viktoria Demidovich <88316755+ViktoriaDem@users.noreply.github.com>
2022-09-28 00:37:21 +0300

    Add files via upload

---
+++ js/slider.js
@@ -0,0 +1,13 @@
+new Swiper('.image-slider', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+
+  },
+  slidesPerView: 1,
+});
